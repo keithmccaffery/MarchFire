c8cf7a847df553aa8b1ff160fb16bb099c715ae7
keithmccaffery <132424932+keithmccaffery@users.noreply.github.com>
2024-05-29 14:16:44 +1000

    Added to requirements.txt v7

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ import json
 import pyodbc
 from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
+from azure.storage.blob import BlobServiceClient
 from MsSql import MsSql
 
 from cs50 import SQL
